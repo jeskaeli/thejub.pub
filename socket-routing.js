bf3833b1e966d7663497490a5566b49ee838f9e4
aromatt <amatteson3@gmail.com>
2015-10-01 23:28:22 -0400

    Added chat emoting with /me

--- socket-routing.js
+++ socket-routing.js
@@ -1,5 +1,12 @@
 var util = require('util');
 
+// patch String with #startsWith
+if ( typeof String.prototype.startsWith != 'function' ) {
+  String.prototype.startsWith = function( str ) {
+    return this.substring( 0, str.length ) === str;
+  }
+};
+
 function emit_users(io, users) {
   io.emit('current users', users.toArray());
 }
@@ -12,6 +19,26 @@ function refresh_users(io, jub) {
   console.log('refresh users');
   jub.clear_users();
   io.emit('ping users', 0); // Tell the users to report their presence
+}
+
+// Take in a chat obj sent from the client and turn into something we can
+// emit to display in the chat history
+function transform_chat(msg_obj) {
+  var formatted = msg_obj['text'];
+  var emph = false;
+  if (msg_obj['username']) {
+    if (formatted.startsWith('/me ')) {
+      formatted = '* ' + msg_obj['username'] + ' ' +
+                  formatted.substring(3, formatted.length);
+      emph = true;
+    } else {
+      formatted = msg_obj['username'] + ": " + formatted;
+    }
+  }
+  return {
+    text: formatted,
+    emph: emph
+  };
 }
 
 function Socketeer(jub, config, io) {
@@ -42,14 +69,10 @@ function Socketeer(jub, config, io) {
     });
 
     // Chat message received
-    socket.on('chat message', function(msg) {
+    socket.on('chat message', function(client_msg_obj) {
       // Echo message back to clients
-      if (msg['text']) {
-        var formatted = msg['text']
-        if (msg['username']) {
-          formatted = msg['username'] + ": " + formatted;
-        }
-        emit_chat(io, formatted);
+      if (client_msg_obj['text']) {
+        emit_chat(io, transform_chat(client_msg_obj));
       }
     });
 
