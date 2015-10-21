5491bf3a2a2c98e9bc4114e86802a026593ea464
Andrew Matteson <amatteson3@gmail.com>
2015-10-20 23:30:20 -0400

    Working on client-side playlists

--- public/javascripts/gapi.js
+++ public/javascripts/gapi.js
@@ -47,3 +47,40 @@ var youtube_video_specs = function(obj, callback) {
     });
   }
 }
+
+// Returns an array of result items with this structure:
+//   https://developers.google.com/youtube/v3/docs/search/list#response
+var youtube_playlist = function(id, callback, page_token, list_prefix) {
+  var params = {
+    part: 'id,snippet',
+    maxResults: 50,      // this is the maximum allowed
+    playlistId: id,
+    auth: gapi_key
+  };
+
+  list_prefix = list_prefix || [];
+  if (page_token) { params.pageToken = page_token }
+
+  gapi.client.youtube.playlistItems.list(params).then(function(resp) {
+    console.log(resp);
+    var next_page_token = resp.nextPageToken;
+    if (resp.result && resp.result.items && resp.result.items.length > 0) {
+      var video_list = resp.result.items.map(function(item) {
+        return { title: item.snippet.title,
+          id: item.snippet.resourceId.videoId,
+        position: item.snippet.position
+        }
+      });
+
+      // If there's a next page, recursively call playlist() until there's
+      // no next page. Then finally call the callback
+      if (next_page_token) {
+        console.log('fetching next page of playlist', next_page_token);
+        youtube_playlist(id, callback, next_page_token,
+          list_prefix.concat(video_list));
+      } else {
+        callback(list_prefix.concat(video_list));
+      }
+    }
+  });
+}
