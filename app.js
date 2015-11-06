9b3ad5ea2ad379b5b7b7752f5c98bdfb8e39da15
aromatt <amatteson3@gmail.com>
2015-11-06 02:23:43 -0500

    Trying a workaround for favicon

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.locals.pretty = true;
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -54,6 +54,12 @@ app.get('/test_path', function(req, res, next) {
       res.send(html);
     }
   });
+});
+
+// favicon workaround for shitty DNS provider who won't forward arbitrary
+// paths and won't let me set an explicit rule for a path with '.' in it
+app.get('/favicon', function(req, res, next) {
+  res.redirect('/favicon.ico');
 });
 
 // Minimal message at '/' route
